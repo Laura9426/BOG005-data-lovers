79e9479814db09a2077deef4e96d46542ce79d64
angelicamcs12 <angelicamariachs@gmail.com>
2022-08-18 18:26:56 -0500

    actualizacion

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { Cartas} from './data.js';
 document.getElementById("root").appendChild(Cartas());
 
-
-
+const boton = document.getElementById('boton1');
+console.log(boton)
+boton.addEventListener('click', function (){
+    alert("Página en construcción")
+    alert("coso")
+});
